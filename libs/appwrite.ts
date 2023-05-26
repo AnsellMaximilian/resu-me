0257ff7ceba6eaa7054dd1d94eadf21c03068050
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-26 09:43:21 +0700

    Modify appwrite implementation

--- libs/appwrite.ts
+++ libs/appwrite.ts
@@ -1,8 +1,10 @@
-import { Client } from "appwrite";
+import { Account, Client } from "appwrite";
 const client = new Client();
 
 client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
   .setProject(process.env.NEXT_PUBLIC_PROJECT_ID as string);
+
+export const account = new Account(client);
 
 export default client;
