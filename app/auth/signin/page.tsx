c6284070fd228c920c00fa36838bdab6969ddd56
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-26 10:11:47 +0700

    Apply primary color

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -35,7 +35,7 @@ const SignUpPage = () => {
   };
   return (
     <div>
-      <section className="bg-gray-50">
+      <section className="bg-primary-main">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
