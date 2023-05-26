0cf5bec05f92247628131bbbd3567a1a596d3257
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-26 09:45:07 +0700

    Add app page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,33 +1,13 @@
-"use client";
-import { useRouter } from "next/navigation";
-import client from "@/libs/appwrite";
+import client, { account } from "@/libs/appwrite";
 import { Account, Models } from "appwrite";
 
 import AppHeader from "@/components/AppHeader";
 import { useEffect, useState } from "react";
 
-const AppPage = async () => {
-  const router = useRouter();
-  const [currentAccount, setCurrentAccount] =
-    useState<Models.User<Models.Preferences> | null>(null);
-  useEffect(() => {
-    const account = new Account(client);
-    const promise = account.get();
-    promise
-      .then((account) => setCurrentAccount(account))
-      .catch((error) => {
-        router.push("/auth/signin");
-      });
-  }, [router]);
+export default function AppPage() {
   return (
     <div>
-      {currentAccount && (
-        <div>
-          <AppHeader currentAccount={currentAccount} />
-        </div>
-      )}
+      <AppHeader />
     </div>
   );
-};
-
-export default AppPage;
+}
