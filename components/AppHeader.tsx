9036d1337c0847334cdf50a11321386f72b80250
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-26 09:43:31 +0700

    Add appheader

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,14 +1,37 @@
-import { Models } from "appwrite";
+"use client";
 
-export default function AppHeader({
-  currentAccount,
-}: {
-  currentAccount: Models.User<Models.Preferences>;
-}) {
+import useAuth from "@/hooks/useAuth";
+import { Models } from "appwrite";
+import Link from "next/link";
+import ActiveLink from "./ActiveLink";
+
+export default function AppHeader() {
+  const { currentAccount, logout } = useAuth();
   return (
-    <header>
-      <nav>
-        <div>{currentAccount.name}</div>
+    <header className="py-3 px-4 shadow-md">
+      <nav className="flex justify-between items-center">
+        <div className="flex gap-8 items-center">
+          <div className="font-bold text-lg">Resu-Me</div>
+          <div>
+            <ul className="flex gap-4 items-center text-sm">
+              <li>
+                <ActiveLink label="Dashboard" href="/app" />
+              </li>
+              <li>
+                <ActiveLink label="Resume" href="/app/resume" />
+              </li>
+              <li>
+                <ActiveLink label="Groups" href="/app/groups" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <button
+          onClick={logout}
+          className="bg-secondary-main text-white px-4 py-1 rounded-md hover:bg-secondary-dark text-sm"
+        >
+          Logout
+        </button>
       </nav>
     </header>
   );
