0478430d292acd0a9705bb4561b969477a5f89a3
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-27 16:40:53 +0700

    Add custom skill function

---
+++ functions/submitCustomSkill/src/index.js
@@ -0,0 +1,65 @@
+const sdk = require("node-appwrite");
+
+/*
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - request body data as a string
+    'variables' - object with function variables
+
+  'res' variable has:
+    'send(text, status)' - function to return text response. Status code defaults to 200
+    'json(obj, status)' - function to return JSON response. Status code defaults to 200
+
+  If an error is thrown, a response with code 500 will be returned.
+*/
+
+module.exports = async function (req, res) {
+  const client = new sdk.Client();
+
+  // You can remove services you don't use
+  const account = new sdk.Account(client);
+  const database = new sdk.Databases(client);
+  const functions = new sdk.Functions(client);
+
+  if (
+    !req.variables["APPWRITE_FUNCTION_ENDPOINT"] ||
+    !req.variables["APPWRITE_FUNCTION_API_KEY"]
+  ) {
+    console.warn(
+      "Environment variables are not set. Function cannot use Appwrite SDK."
+    );
+  } else {
+    client
+      .setEndpoint(req.variables["APPWRITE_FUNCTION_ENDPOINT"])
+      .setProject(req.variables["APPWRITE_FUNCTION_PROJECT_ID"])
+      .setKey(req.variables["APPWRITE_FUNCTION_API_KEY"])
+      .setSelfSigned(true);
+
+    const { name } = JSON.parse(req.payload);
+
+    // Check if document exists
+    const skills = await database.listDocuments(
+      req.variables["DATABASE_ID"],
+      req.variables["SKILL_COLLECTION_ID"],
+      [sdk.Query.equal("name", [name])]
+    );
+
+    // if true return document
+    console.log("skills", skills);
+
+    if (skills.total > 0) res.json(skills.documents[0]);
+
+    // else create and return
+    const customSkill = await database.createDocument(
+      req.variables["DATABASE_ID"],
+      req.variables["SKILL_COLLECTION_ID"],
+      name,
+      { name, approved: false }
+    );
+    res.json(customSkill);
+  }
+  console.log("hello");
+  res.json({
+    areDevelopersAwesome: true,
+  });
+};
