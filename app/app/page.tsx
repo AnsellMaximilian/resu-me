b73dbaa6eeaaccfef3c4d8ac765b1ef574de33b1
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-05-31 10:18:16 +0700

    Add search bar

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -9,8 +9,22 @@ import ResumeList from "@/components/ResumeList";
 import { ToastContainer } from "react-toastify";
 
 export default function AppPage() {
+  const [titleSearch, setTitleSearch] = useState("");
   return (
     <div className="p-4">
+      <div className="mb-4">
+        <div>
+          <input
+            value={titleSearch}
+            onChange={(e) => setTitleSearch(e.target.value)}
+            type="search"
+            name="search-title"
+            id="search-title"
+            className="input rounded-full"
+            placeholder="Search for resume title"
+          />
+        </div>
+      </div>
       <ResumeList />
       <ToastContainer />
     </div>
